699dfa4bcad14d199ec9c920a1592eeddb4c0e63
Oscar Montoya <omontoya@unomaha.edu>
2018-02-18 22:59:31 -0600

    Added Course class.

---
+++ ClassroomAssignment/ClassroomAssignment/Model/Course.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassroomAssignment.Model
+{
+    class Course
+    {
+
+        // The Names of these properties match that of the departmental headers
+        private int CourseID { get; set; }
+        private int SIS_ID { get; set; }
+        private String Term { get; set; } 
+        private int TermCode { get; set; }
+        private String DepartmentCode { get; set; }
+        private String SubjectCode { get; set; }
+        private int CatalogNumber { get; set; }
+        private String Course { get; set; }
+        private String 
+    }
+}
