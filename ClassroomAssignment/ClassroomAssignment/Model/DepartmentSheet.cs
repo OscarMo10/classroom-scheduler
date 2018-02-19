c187ebb6d46e5a46ffc02574be1ba8a93c2bb3c4
Oscar Montoya <omontoya@unomaha.edu>
2018-02-19 16:51:40 -0600

    Added to DepartmentSheet class.

--- ClassroomAssignment/ClassroomAssignment/Model/DepartmentSheet.cs
+++ ClassroomAssignment/ClassroomAssignment/Model/DepartmentSheet.cs
@@ -10,7 +10,31 @@ namespace ClassroomAssignment.Model
     {
         public class Headers
         {
+            public static string CLASS_ID = "CLSS ID";
+            public static string SESSION_ID = "SIS ID";
+            public static string TERM = "Term";
+            public static string DEPARTMENT_CODE = "Department Code";
+            public static string SUBJECT_CODE = "Subject Code";
+            public static string CATALOG_NUMBER = "Catalog Number";
+            public static string COURSE = "Course";
+            public static string SECTION_NUM = "Section #";
+            public static string COURSE_TITLE = "Course Title";
+            public static string SECTION_TYPE = "Section Type";
+            public static string TITLE_SLASH_TOPIC = "Title"
 
+        }
+
+        public class SectionTypeOptions
+        {
+            public static string LECTURE = "Lecture";
+            public static string INDEPENDENT_STUDY = "Independent Study";
+            public static string STUDIO = "Studio";
+        }
+
+        public class MeetingPatternOptions
+        {
+
+            public static string DOES_NOT_MEET = "Does Not Meet";
         }
     }
 }
