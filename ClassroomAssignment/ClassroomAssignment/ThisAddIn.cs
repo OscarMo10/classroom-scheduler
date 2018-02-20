703d5abbd796030a3eb009bf2077709b52ec8e5d
Celal Gorgun <cgorgun@unomaha.edu>
2018-02-20 11:43:30 -0600

    updated (Combine Data)
    I tested it works for now. I just created Celal1, Celal2, Celal3 excel spreadsheets and combine them into one. I used Spire.XLS library to combine

--- ClassroomAssignment/ClassroomAssignment/ThisAddIn.cs
+++ ClassroomAssignment/ClassroomAssignment/ThisAddIn.cs
@@ -1,4 +1,5 @@
-﻿using System;
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,6 +7,8 @@ using System.Xml.Linq;
 using Excel = Microsoft.Office.Interop.Excel;
 using Office = Microsoft.Office.Core;
 using Microsoft.Office.Tools.Excel;
+using System.Windows.Forms;
+using Spire.Xls;
 
 namespace ClassroomAssignment
 {
@@ -14,6 +17,24 @@ namespace ClassroomAssignment
         private void ThisAddIn_Startup(object sender, System.EventArgs e)
         {
             this.Application.WorkbookBeforeSave += new Microsoft.Office.Interop.Excel.AppEvents_WorkbookBeforeSaveEventHandler(Application_WorkbookBeforeSave);
+              Workbook MerBook = new Workbook();
+            MerBook.LoadFromFile("Celal1.xlsx");
+            Worksheet MerSheet = MerBook.Worksheets[0];
+
+            Workbook SouBook1 = new Workbook();
+            SouBook1.LoadFromFile("Celal2.xlsx");
+            int a = SouBook1.Worksheets[0].LastRow;
+            int b = SouBook1.Worksheets[0].LastColumn;
+            SouBook1.Worksheets[0].Range[2, 1, a, b].Copy(MerSheet.Range[MerSheet.LastRow + 1, 1, a + MerSheet.LastRow, b]);
+
+
+            Workbook SouBook2 = new Workbook();
+            SouBook2.LoadFromFile("Celal3.xlsx");
+            int c = SouBook2.Worksheets[0].LastRow;
+            int d = SouBook2.Worksheets[0].LastColumn;
+            SouBook2.Worksheets[0].Range[2, 1, c, d].Copy(MerSheet.Range[MerSheet.LastRow + 1, 1, c + MerSheet.LastRow, d]);
+
+            MerBook.SaveToFile("result.xlsx", ExcelVersion.Version2010);
         }
 
         private void ThisAddIn_Shutdown(object sender, System.EventArgs e)
