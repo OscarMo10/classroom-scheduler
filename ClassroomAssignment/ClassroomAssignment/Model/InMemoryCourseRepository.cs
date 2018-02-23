4ada5f4b3ac7a5fe1cc1647b7d38be605866d9ce
Oscar Montoya <omontoya@unomaha.edu>
2018-02-22 20:10:24 -0600

    Added classes for in-memory repositories for rooms and courses.

---
+++ ClassroomAssignment/ClassroomAssignment/Model/InMemoryCourseRepository.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassroomAssignment.Model
+{
+    class InMemoryCourseRepository : ICourseRepository
+    {
+        public string Term { get; }
+
+        public List<Course> Courses { get; }
+
+        public InMemoryCourseRepository(string term, List<Course> courses)
+        {
+            this.Term = term;
+            this.Courses = courses;
+        }
+    }
+}
---
+++ ClassroomAssignment/ClassroomAssignment/Model/InMemoryRoomRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassroomAssignment.Model
+{
+    class InMemoryRoomRepository : IRoomRepository
+    {
+        public string getNormalizedRoomName(string roomName)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
